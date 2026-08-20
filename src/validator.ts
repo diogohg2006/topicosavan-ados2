9194a74ad547cf7748131a0a8c09dc155db2af76
DIOGO HENRIQUE GIORDANO <diogo.giordano@cs.cruzeirodosul.edu.br>
2026-08-19 22:03:37 -0300

    teste certo

--- src/validator.ts
+++ src/validator.ts
@@ -2,14 +2,14 @@ function validarChamado(titulo: string): boolean {
     if (!titulo  || titulo.trim() === "") {
         throw new Error("O título do chamado não pode ser vazio.");
     }
-    return true;
+   
 if (titulo.trim().length < 5) {
         throw new Error("O título deve possuir no minimo 5 caracters.");
     }
 
     if (titulo.trim().length > 100) {
-        throw new Error("O título deve possuir no máximo 100.");
+        throw new Error("O título deve possuir no máximo 100 caracters..");
     }
-
+ return true;
 }
 export default validarChamado;
