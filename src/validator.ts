a2ed17cc71c6248bc2db995c1250f647d0918a34
DIOGO HENRIQUE GIORDANO <diogo.giordano@cs.cruzeirodosul.edu.br>
2026-08-19 21:57:24 -0300

    teste certo

--- src/validator.ts
+++ src/validator.ts
@@ -7,8 +7,8 @@ if (titulo.trim().length < 5) {
         throw new Error("O título deve possuir no minimo 5 caracters.");
     }
 
-    if (titulo.trim()) {
-        throw new Error("O título deve possuir no máximo 100 caracters.");
+    if (titulo.trim().length > 100) {
+        throw new Error("O título deve possuir no máximo.");
     }
 
 }
